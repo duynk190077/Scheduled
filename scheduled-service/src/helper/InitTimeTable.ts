07125dd31eb99330a85995dbeda1921aed7de122
duynk190077 <duypro10x123@gmail.com>
2023-01-11 15:28:45 +0700

    back-feature/scheduled: update scheduled task

---
+++ scheduled-service/src/helper/InitTimeTable.ts
@@ -0,0 +1,13 @@
+export const InitTimetable = (room: number, day: number, lessonPerday: number) => {
+    let timetable = [];
+    for (let i = 0; i < room; i++) {
+      timetable[i] = [];
+      for (let j = 2; j <= day + 1; j++) {
+        timetable[i][j] = [];
+        for (let k = 1; k <= lessonPerday; k++) {
+          timetable[i][j][k] = true;
+        }
+      }
+    }
+    return timetable;
+}
